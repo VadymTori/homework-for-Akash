c65854e442f1e500e72b9f86d2cf3e14da9dc2e3
VadymTori <ivanovvad90@gmail.com>
2026-02-04 12:07:32 -0500

    Bugs fixed

---
+++ dz/Rezume springboard/REAMDE.md
@@ -0,0 +1,42 @@
+# Project's Title. This is the name of the project. ...
+Vadym Ivanov portfolio website
+
+# Project Description. This is an important component of your project that many new developers often overlook. ...
+tHIS WOULD BE MY REAL WEBSITE
+
+To do:
+# Four pages for website: [ ]
+# Main Page [X]
+Includes about info [X]
+Ways to contact me: telegram, facebook etc...[X]
+My work exp [X]
+Education [X]
+Skills [X]
+# Portfolio [ ]
+QA Portfolio [X]
+Web dev portfolio [ ]
+-add picasso website [ ]
+-add shelter enter JS website [ ]
+# Download CV [X]
+Add file to download (PDF) [X]
+# Contact form [ ]
+Use JavaScript [X]
+Creat spec log file to check  msgs [ ]
+
+# How to Install and Run the Project. ...
+Buy a computer, install OS, run browser, open webpage.... make a month payment for enternet to your provider
+
+# How to Use the Project. ...
+Use it for your own risk
+
+# Include Credits. ...
+Nah
+
+# Add a License. ...
+Driver license?
+
+# Badges.
+Bad what?
+
+# Testing
+Testing is my priority...
---
+++ dz/Rezume springboard/src/react.js
@@ -0,0 +1,40 @@
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://httpbin.org/post'
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
+
+    }
+    )
+})
