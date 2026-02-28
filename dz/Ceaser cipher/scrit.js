19b645e59fe00051ef065fe7c634bc9526d41cc3
VadymTori <ivanovvad90@gmail.com>
2026-02-28 15:34:30 -0500

    New task Ceaser Cipheet added (not finished)

---
+++ dz/Ceaser cipher/scrit.js
@@ -0,0 +1,11 @@
+const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+
+        function showText1() {
+            document.getElementById("secret-text1").classList.remove("hidden");
+        }
+                function showText2() {
+            document.getElementById("secret-text2").classList.remove("hidden");
+        }
+                        function showText3() {
+            document.getElementById("secret-text3").classList.remove("hidden");
+        }
