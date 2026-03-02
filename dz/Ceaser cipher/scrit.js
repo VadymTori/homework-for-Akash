0644dc2801f7d27c5b82a323ea609f9744c26f1b
VadymTori <ivanovvad90@gmail.com>
2026-03-02 10:45:59 -0500

    Task completed

--- dz/Ceaser cipher/scrit.js
+++ dz/Ceaser cipher/scrit.js
@@ -1,4 +1,5 @@
-const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+let guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
+// I changed const to let only because of last task
 
         function showText1() {
             document.getElementById("secret-text1").classList.remove("hidden");
@@ -9,3 +10,17 @@ const guests = ["ANTONY", "CICERO", "CASSIUS", "CLEOPATRA"];
                         function showText3() {
             document.getElementById("secret-text3").classList.remove("hidden");
         }
+                                function showText4() {
+            document.getElementById("secret-text4").classList.remove("hidden");
+        }
+                                        function showText5() {
+            document.getElementById("secret-text5").classList.remove("hidden");
+        }
+                                                function showText6() {
+            document.getElementById("secret-text6").classList.remove("hidden");
+        }
+                                                        function showText7() {
+            document.getElementById("secret-text7").classList.remove("hidden");
+        }
+
+// All this just to make webpage more interractive))
