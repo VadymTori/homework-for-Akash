e67ad784ebf5f7c0d6fae3a6e93dcfeef3a0dbb6
VadymTori <ivanovvad90@gmail.com>
2026-03-04 14:53:13 -0500

    Task 7 added. Main page updated

---
+++ dz/Exercise 7/script.js
@@ -0,0 +1,26 @@
+// const friend = "BRUTUS"
+// const shiftValue = 3;
+
+// const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// ----------------------------------------------------
+function makeSecure() {
+    const input = document.getElementById("inputText").value;
+    let result = "";
+
+    for (let i = 0; i < input.length; i++) {
+        let char = input[i];
+
+        if (char >= 'a' && char <= 'z') {
+            result += String.fromCharCode((char.charCodeAt(0) - 97 + 3) % 26 + 97);
+        } 
+        else if (char >= 'A' && char <= 'Z') {
+            result += String.fromCharCode((char.charCodeAt(0) - 65 + 3) % 26 + 65);
+        } 
+        else {
+            result += char;
+        }
+    }
+
+    document.getElementById("outputText").value = result.toLowerCase();
+}
